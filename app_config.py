58dffbbef22c16d3f4126c3483af7e82e4db7dc9
salil-cervello <62046720+salil-cervello@users.noreply.github.com>
2020-03-24 10:30:18 +0530

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,7 @@
+import os
+
+CLIENT_SECRET = <from App Registration secret key>
+AUTHORITY = "https://login.microsoftonline.com/68932627-def8-4e45-ac72-14df51d766c2"
+CLIENT_ID = <from App Registration Cleint id>
+REDIRECT_PATH = "/getAToken"
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'
